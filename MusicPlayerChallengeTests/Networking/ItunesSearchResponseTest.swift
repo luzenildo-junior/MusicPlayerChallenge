0fccee690960e21df36f8a9ba77d0dbe5e384a67
Luzenildo Junior <luzejunior@gmail.com>
2023-09-22 23:21:14 -0300

    More tests added for itunes search response

--- MusicPlayerChallengeTests/Networking/ItunesSearchResponseTest.swift
+++ MusicPlayerChallengeTests/Networking/ItunesSearchResponseTest.swift
@@ -9,7 +9,23 @@
 import XCTest
 
 final class ItunesSearchResponseTest: XCTestCase {
-    func testModelParse() {
+    func testModelParse_withSearchTerm() {
+        // Given
+        let parsedData: ItunesSearchResponse = "SearchResult".decodeJSONFromFileName()
+        
+        // When
+        let parsedObject = parsedData.results[0]
+        
+        // Then
+        XCTAssertEqual(parsedObject.artistName, "Iron Maiden")
+        XCTAssertNotNil(parsedObject.artworkUrl100)
+        XCTAssertEqual(parsedObject.collectionName, "Fear of the Dark")
+        XCTAssertEqual(parsedObject.trackName, "Fear of the Dark")
+        XCTAssertNotNil(parsedObject.collectionId)
+        XCTAssertEqual(parsedObject.trackTimeMillis, 438114)
+    }
+    
+    func testModelParse_withAlbumFetch() {
         // Given
         let parsedData: ItunesSearchResponse = "FetchAlbumResult".decodeJSONFromFileName()
         
